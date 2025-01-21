a25790d53905e01fd3d3ab8084facae1a538b5ac
jiabaow <jiabaowenwendy@gmail.com>
2025-01-20 21:00:40 -0800

    init commit with basic checkbox behavior

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+//import { useState } from 'react'
+import './App.css'
+import CheckboxItem from "./CheckboxItem";
+
+const data = [
+  {
+    name: "US",
+    children: [
+      {
+        name: "CA",
+        children: [
+          { name: "Irvine" },
+          { name: "LA" }
+        ]
+      }
+    ]
+  }
+];
+
+function App() {
+
+  return (
+    <div style={{ padding: '20px'}}>
+      { data.map((item, index) => (
+          <CheckboxItem key={index} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default App
---
+++ src/CheckboxItem.jsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+const CheckboxItem = ({ item, level = 0 }) => {
+    const [checked, setChecked] = useState(false);
+
+    const hasChildren = item.children && item.children.length > 0;
+
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+
+    return (
+        <div style={{ marginLeft: level * 20 }}>
+            <label>
+                <input type="checkbox" checked={checked} onChange={handleChange}/>
+                {item.name}
+            </label>
+            { hasChildren &&
+                item.children.map((child, index) => (
+                    <CheckboxItem key={index} item={child} level={level+1} />
+                ))}
+        </div>
+    );
+};
+
+export default CheckboxItem;
